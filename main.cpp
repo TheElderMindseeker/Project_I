6be260ec0b35c251a41dce46a609ffcaa684bf7a
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2017-09-20 17:23:23 +0300

    Task 3 solved

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ const int CURRENT_YEAR = 2017;
 
 void sentence_information_amount (std::vector<Student> &team);
 size_t calculate_text_entropy (std::vector<Student> &team);
+void calculate_watch_choice_probability (size_t M);
 
 float question_information (const std::vector<Student> &team, const Date question_date);
 float phrase_entropy (const std::string phrase);
@@ -28,6 +29,7 @@ int main (int argc, char **argv) {
 
     sentence_information_amount (team);
     size_t M = calculate_text_entropy (team);
+    calculate_watch_choice_probability (M);
 
     return 0;
 }
@@ -35,20 +37,20 @@ int main (int argc, char **argv) {
 
 void sentence_information_amount (std::vector<Student> &team) {
     std::string today_s;
-    std::cout << "Input current date (in a format dd.mm.yyyy): ";
-    std::cin >> today_s;
 
     Date today;
 
     bool valid_input;
     do {
+        std::cout << "Input current date (in a format dd.mm.yyyy): ";
+        std::cin >> today_s;
         valid_input = true;
+
         try {
             today = Date (today_s);
         }
         catch (std::invalid_argument e) {
             std::cout << e.what () << '\n';
-            std::cout << "Input current date (in a format dd.mm.yyyy): ";
             valid_input = false;
         }
     }
@@ -102,6 +104,26 @@ size_t calculate_text_entropy (std::vector<Student> &team) {
               << phrase_entropy (text_generator.str ()) << std::endl;
 
     return text_generator.str ().length ();
+}
+
+
+void calculate_watch_choice_probability (size_t M) {
+    const unsigned N = 120;
+
+    unsigned orient = M,
+            casio = N + M,
+            swatch = 2 * M;
+    unsigned total = orient + casio + swatch;
+
+    float first_orient = (float) orient / total;
+    float cond_second_swatch = (float) swatch / (total - 1);
+
+    std::cout << "\nThe probability of picking Orient as the first model is "
+              << std::setiosflags (std::ios_base::showpoint | std::ios_base::fixed) << std::setprecision (2)
+              << first_orient << ".\n";
+    std::cout << "The probability of picking Swatch as the second model given that we've picked Orient as first is "
+              << std::setiosflags (std::ios_base::showpoint | std::ios_base::fixed) << std::setprecision (2)
+              << cond_second_swatch << ".\n";
 }
 
 
